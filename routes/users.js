4b75570c1e87c503069249c621a42c546fa44fdc
Ruffnex92 <m.hajok@gmx.net>
2017-04-26 20:42:12 +0200

    added first database functions and routing

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../server/knexdb');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('welcome to users page');
+});
+
+router.get('/create/:username/:age/:country', function (req, res, next) {
+    var username = req.params['username'];
+    var age = req.params['age'];
+    var country = req.params['country'];
+    knex.createUser(username, age, country)
+        .then(function () {
+            res.send('user created successfully');
+        }, function (err) {
+            res.send('cant create user. ' + err.toString());
+        });
+});
+
+router.get('/show', function (req, res, next) {
+    knex.getUsers().then(function (users) {
+        res.send(users);
+    })
+});
+
+
+module.exports = router;
---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var knexdb = require('../server/knexdb');
+
+/*describe('knex', function() {
+    describe('#createUser()', function() {
+        it('should return true when created successfully', function() {
+            assert.equal(true, knexdb.createUser('matthias', 12, 'deutschland'));
+        });
+    });
+});*/
+
