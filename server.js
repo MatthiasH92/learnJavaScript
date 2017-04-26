aab3f8f6aff61902c28963ff2f2594e1900e5f7d
Ruffnex92 <m.hajok@gmx.net>
2017-04-26 20:54:27 +0200

    added first database functions and routing

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var routes = require('./routes/index');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
 app.use('/test', express.static(process.cwd() + '/test'));
 app.use('/server', express.static(process.cwd() + '/server'));
 
 routes(app);
 
-app.listen(3000, function () {
+app.listen(5000, function () {
     console.log("listening on port 3000");
     var test = require(process.cwd() + '/test/manual');
     test.runTests();
